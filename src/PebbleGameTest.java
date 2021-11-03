1bfcde4bc721affa1c72400eab97f31526763d23
Jack Shaw <73115676+js1294@users.noreply.github.com>
2021-11-03 15:23:36 +0000

    Update PebbleGameTest.java

--- src/PebbleGameTest.java
+++ src/PebbleGameTest.java
@@ -1,42 +1,22 @@
 package src;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 
-import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.*;
-
+import java.util.InputMismatchException;
+import java.util.ArrayList;
 import src.PebbleGame.Player;
 
 public class PebbleGameTest {
+
+
+  @BeforeClass
+  public static void setUpClass(){
+  }
   @Test
-  public void playerTest(){
-    Player player = new Player();
-    ArrayList<Integer> pebbles = new ArrayList<>();
-
-    //Testing initial values.
-    assertEquals(pebbles, player.getPebbles());
-    assertEquals(0, player.getBlackNumber());
-    assertEquals(0,player.getTotalWeight());
-    assertEquals(0, player.getChoice());
-
-    //Testing adding a pebble.
-    player.addPebble(20);
-    pebbles.add(20);
-    assertEquals(pebbles, player.getPebbles());
-    assertEquals(20, player.getTotalWeight());
-
-    //Testing removing a pebble.
-    player.removePebble(20);
-    pebbles.remove(0);
-    assertEquals(pebbles, player.getPebbles());
-    assertEquals(0, player.getTotalWeight());
-
-    //Testing removing from an empty list
-    player.removePebble(20);
-    assertEquals(pebbles, player.getPebbles());
-    assertEquals(0, player.getTotalWeight());
+  public void pebbleGameTest(){
+    
   }
 }
-
 
