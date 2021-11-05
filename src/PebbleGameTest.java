7e6662b864916a79b8d5f772d62fe22cbd2a9a6d
Jack Shaw <73115676+js1294@users.noreply.github.com>
2021-11-05 15:18:03 +0000

    Added Drawer Testing

--- src/PebbleGameTest.java
+++ src/PebbleGameTest.java
@@ -1,5 +1,6 @@
 package src;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -7,15 +8,20 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 
+import src.PebbleGame.Player;
+
 
 public class PebbleGameTest {
 
   private static PebbleGame pebbleGame;
+
+  private static Player player;
 
   private String validFile;
 
@@ -31,6 +37,8 @@ public class PebbleGameTest {
   @BeforeClass
   public static void setup(){
     pebbleGame = new PebbleGame();
+    player = new Player();
+    pebbleGame.setNumPlayers(1);
   }
 
   @Before
@@ -58,6 +66,90 @@ public class PebbleGameTest {
     } catch (IOException e) {
       fail("IOException");
     }
+  }
+
+  @Test
+  public void drawerTest(){
+    ArrayList<ArrayList<AtomicInteger>> blackBags = new ArrayList<>(3);
+    ArrayList<ArrayList<AtomicInteger>> whiteBags = new ArrayList<>(3);
+
+    ArrayList<AtomicInteger> validBag = new ArrayList<>(Arrays.asList(new AtomicInteger(1),
+    new AtomicInteger(2), new AtomicInteger(3), new AtomicInteger(4),
+    new AtomicInteger(5), new AtomicInteger(6), new AtomicInteger(7),
+    new AtomicInteger(8),new AtomicInteger(9), new AtomicInteger(10),
+    new AtomicInteger(11)));
+
+    // Test with all black bags filled and white bags empty.
+    blackBags.add(validBag);
+    blackBags.add(validBag);
+    blackBags.add(validBag);
+
+    whiteBags.add(new ArrayList<>());
+    whiteBags.add(new ArrayList<>());
+    whiteBags.add(new ArrayList<>());
+
+    // Validate length of bags is correct for test.
+    assertEquals(11, blackBags.get(0).size());
+    assertEquals(11, blackBags.get(1).size());
+    assertEquals(11, blackBags.get(2).size());
+    assertEquals(0, whiteBags.get(0).size());
+    assertEquals(0, whiteBags.get(1).size());
+    assertEquals(0, whiteBags.get(2).size());
+    assertEquals(1, pebbleGame.getNumPlayers());
+
+    pebbleGame.setBlackbags(blackBags);
+    pebbleGame.setWhitebags(whiteBags);
+
+    player.setPebbles(new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10)));
+    assertEquals(10, player.getPebbles().size());
+    assertEquals(11, pebbleGame.getBlackbags().get(player.getBlackNumber()).size());
+    assertEquals(0, pebbleGame.getWhitebags().get(player.getBlackNumber()).size());
+
+    pebbleGame.drawer(player);
+
+    assertEquals(11, player.getPebbles().size());
+    assertEquals(10, pebbleGame.getBlackbags().get(player.getBlackNumber()).size());
+    assertEquals(0, pebbleGame.getWhitebags().get(player.getBlackNumber()).size());
+
+    //Test with all white bags filled and black bags empty.
+    validBag = new ArrayList<>(Arrays.asList(new AtomicInteger(1),
+    new AtomicInteger(2), new AtomicInteger(3), new AtomicInteger(4),
+    new AtomicInteger(5), new AtomicInteger(6), new AtomicInteger(7),
+    new AtomicInteger(8),new AtomicInteger(9), new AtomicInteger(10),
+    new AtomicInteger(11)));
+
+    blackBags.clear();
+    blackBags.add(new ArrayList<>());
+    blackBags.add(new ArrayList<>());
+    blackBags.add(new ArrayList<>());
+
+    whiteBags.clear();
+    whiteBags.add(validBag);
+    whiteBags.add(validBag);
+    whiteBags.add(validBag);
+
+    // Validate length of bags is correct for test.
+    assertEquals(0, blackBags.get(0).size());
+    assertEquals(0, blackBags.get(1).size());
+    assertEquals(0, blackBags.get(2).size());
+    assertEquals(11, whiteBags.get(0).size());
+    assertEquals(11, whiteBags.get(1).size());
+    assertEquals(11, whiteBags.get(2).size());
+    assertEquals(1, pebbleGame.getNumPlayers());
+
+    pebbleGame.setBlackbags(blackBags);
+    pebbleGame.setWhitebags(whiteBags);
+
+    player.setPebbles(new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10)));
+    assertEquals(10, player.getPebbles().size());
+    assertEquals(0, pebbleGame.getBlackbags().get(player.getBlackNumber()).size());
+    assertEquals(11, pebbleGame.getWhitebags().get(player.getBlackNumber()).size());
+
+    pebbleGame.drawer(player);
+
+    assertEquals(11, player.getPebbles().size());
+    assertEquals(10, pebbleGame.getBlackbags().get(player.getBlackNumber()).size());
+    assertEquals(0, pebbleGame.getWhitebags().get(player.getBlackNumber()).size());
   }
 
   @Test
@@ -92,7 +184,7 @@ public class PebbleGameTest {
       fail("Invalid Exception");
     } catch (TotalTooLowException e){}
 
-    ///Testing using hundred players, csv file which would result in no player being able to win.
+    //Testing using hundred players, csv file which would result in no player being able to win.
     try {
       pebbleGame.setNumPlayers(100);
       pebbleGame.reader(tooLowFile);
