7d9e07a37445d26c5dadd8d362662f5f76323f77
Jack Shaw <73115676+js1294@users.noreply.github.com>
2021-11-09 12:51:31 +0000

    Stuff to run the tests

---
+++ src/TestSuit.java
@@ -0,0 +1,13 @@
+package src;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   PebbleGameTest.class,
+   PlayerTest.class
+})
+
+public class TestSuit {}
